905ef0b13c074dbf30a7267a11133040e15276db
Bogdan Ovsiannikov <takahawkkun@gmail.com>
2023-11-11 16:30:48 +0200

    Add initialization with random color

--- actors/Bot.gd
+++ actors/Bot.gd
@@ -1,4 +1,16 @@
 extends Sprite2D
 
+var rng = RandomNumberGenerator.new()
+
 func _ready():
-	pass
+	material.set_shader_parameter("bot_color", random_color())
+	
+func random_color():
+	var color
+	match (rng.randi() % 3):
+		0:
+			return Color.RED
+		1:
+			return Color.GREEN
+		2:
+			return Color.BLUE
