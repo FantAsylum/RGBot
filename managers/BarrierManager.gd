8da086ee9c804e040454757716a8f632acb5ff83
Bogdan Ovsiannikov <takahawkkun@gmail.com>
2023-11-13 10:56:51 +0200

    Implement barrier manager

---
+++ managers/BarrierManager.gd
@@ -0,0 +1,24 @@
+extends Node2D
+
+@export var barrier_scene: PackedScene = preload("res://objects/barrier.tscn")
+
+@export var initial_barrier_count = 3
+
+@onready var current_barrier_position = $CurrentBarrierPosition
+@onready var next_barrier_position = $NextBarrierPosition
+
+var distance_between: float
+
+func _ready():
+	distance_between = next_barrier_position.position.x - current_barrier_position.position.x
+	for i in range(initial_barrier_count):
+		spawn_barrier()
+	
+
+func spawn_barrier():
+	var instance = barrier_scene.instantiate()
+	add_child(instance)
+	
+	instance.position = current_barrier_position.position
+	current_barrier_position.position = next_barrier_position.position
+	next_barrier_position.position.x += distance_between
